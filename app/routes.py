26d6945f9531f71b91cc62c2bf184c79fc703694
thien tran <thanhthientran1018@gamil.com>
2022-11-10 00:05:37 -0800

    style

--- app/routes.py
+++ app/routes.py
@@ -3,9 +3,12 @@ from flask import render_template, flash, redirect, request, url_for
 from app.forms import RequestPing
 
 
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
 def index():
     form = RequestPing()
-
+    if form.validate_on_submit():
+        region = form.rigions.data
+        print(region)
+        
     return render_template('base.html', form = form)
 
