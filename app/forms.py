09ee93d18f506617cd3515521629f67a6feae40d
long nguyen <you@example.com>
2022-11-10 16:40:44 -0800

    more regions

--- app/forms.py
+++ app/forms.py
@@ -3,5 +3,5 @@ from wtforms import SubmitField, SelectField
 from wtforms.validators import DataRequired
 
 class RequestPing(FlaskForm):
-    rigions = SelectField('Regions', choices=[(1, 'NA'),(2, 'Asia'),(3, 'EU'),(4, 'SA')], validators=[DataRequired()])
+    rigions = SelectField('Regions', choices=[(1, 'NA'),(2, 'LAN'),(3, 'EUW'),(4, 'EUNE'),(5,"BR"),(6,"OCE"),(7,"RU")], validators=[DataRequired()])
     submit = SubmitField('Submit')
