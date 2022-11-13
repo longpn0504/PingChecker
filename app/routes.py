ac02b8cd7dd3bcbef60a60bf01bc3b044d0d66b4
tri tran <trittp123@gmail.com>
2022-11-12 22:52:18 -0800

    changed ping function

--- app/routes.py
+++ app/routes.py
@@ -22,19 +22,27 @@ serversDict= {
     7: RU
 }
 def retreivePing(index):
-    server= serversDict[index]
+    server= serversDict[index-1]
     count=0
     responseList=[]
     while(count < 6):
-        ping = os.popen("ping " + server )
+        ping = os.popen("ping " +  server )
         result = ping.readlines()
         msLine = result[-1].strip()
         msLine= msLine[-9: -1]
         msLine= re.sub(r'[^0-9.]', '', msLine)
+        if(re.subr'[^0-9.]', '', msLine
         responseList.append(msLine)
         count = count +1
-    print(responseList)
-    return responseList
+    newList=[
+         (1,responseList[0]),
+         (2,responseList[1]),
+         (3,responseList[2]),
+         (4,responseList[3]),
+         (5,responseList[4])
+         ]
+    print (newList)
+    return newList
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
